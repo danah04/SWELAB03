b64d174b5de9a0b8c61da66bfaa30838bdaac81e
danah04 <danahaldosary1@gmail.com>
2024-09-18 11:21:13 +0300

    rand

--- src/StudentReader.java
+++ src/StudentReader.java
@@ -10,4 +10,15 @@ public class StudentReader {
         studentNames.add("Danah");
         studentNames.add("Hayat");
      }
+    
+    public void createRandomGroups() {        
+        for (int i = 0; i < studentNames.size(); i += 2) {
+            if (i + 1 < studentNames.size()) {
+                // Print a pair
+                System.out.println("Group: " + studentNames.get(i) + ", " + studentNames.get(i + 1));
+            } else {
+                // If odd number of students, the last group will have 3 students
+                System.out.println("Group: " + studentNames.get(i - 1) + ", " + studentNames.get(i));
+            }
+        }
     }
