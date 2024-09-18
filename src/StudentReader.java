e7dd3562b37c0376667f6e6af27f13ab54af706e
danah04 <danahaldosary1@gmail.com>
2024-09-18 11:19:19 +0300

    student

---
+++ src/StudentReader.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+ 
+public class StudentReader {
+     ArrayList<String> studentNames = new ArrayList<>();
+     public void addStudentNames() {
+        studentNames.add("Amal");
+        studentNames.add("Jood");
+        studentNames.add("Noora");
+        studentNames.add("Sara");
+        studentNames.add("Danah");
+        studentNames.add("Hayat");
+     }
